ebc44683df109f786b4f141cbc029e08fd0b01c0
iamthepoe <luca1901p@gmail.com>
2023-05-22 20:22:14 -0300

    create forecast test

---
+++ test/functional/forecast.test.ts
@@ -0,0 +1,43 @@
+const URL = 'http://localhost:3000';
+import assert from 'assert/strict';
+import { describe, it } from "node:test";
+
+describe("Beach forecast functional tests", async ()=>{
+  it("should return status code 200", async ()=>{
+    const {body, status} = await fetch(`${URL}/forecast`);
+    assert.strictEqual(body, [{
+      "time": "2020-04-26T00:00:00+00:00",
+      "forecast": [{
+        "lat": -33.792726,
+        "lng": 151.289824,
+        "name": "Manly",
+        "position": "E",
+        "rating": 2,
+        "swellDirection": 64.26,
+        "swellHeight": 0.15,
+        "swellPeriod": 3.89,
+        "time": "2020-04-26T00:00:00+00:00",
+        "waveDirection": 231.38,
+        "waveHeight": 0.47,
+        "windDirection": 299.45
+      }]},
+      {
+      "time": "2020-04-26T01:00:00+00:00",
+      "forecast": [{
+        "lat": -33.792726,
+        "lng": 151.289824,
+        "name": "Manly",
+        "position": "E",
+        "rating": 2,
+        "swellDirection": 123.41,
+        "swellHeight": 0.21,
+        "swellPeriod": 3.67,
+        "time": "2020-04-26T01:00:00+00:00",
+        "waveDirection": 232.12,
+        "waveHeight": 0.46,
+        "windDirection": 310.48
+      }]}
+    ]);
+    assert.strictEqual(status, 200);
+  });
+});
