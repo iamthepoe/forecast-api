9bdc7d1dd3ab45b5a698a5b4018b5a67f5cdd557
iamthepoe <luca1901p@gmail.com>
2023-05-25 16:31:12 -0300

    add getForecastForLoggedUser function

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,5 +1,8 @@
 import { Controller, Get } from "@overnightjs/core";
+import { Request, Response } from "express";
 
 export class forecastController {
+  public getForecastForLoggedUser(_: Request, res: Response){
 
+  }
 }
