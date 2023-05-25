fdc797a434cf754a0ab4ff8c40f3218be785926a
iamthepoe <luca1901p@gmail.com>
2023-05-25 16:41:01 -0300

    capitalize class name

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from "@overnightjs/core";
 import { Request, Response } from "express";
 
 @Controller('forecast')
-export class forecastController {
+export class ForecastController {
 
   @Get('')
   public getForecastForLoggedUser(_: Request, res: Response){
