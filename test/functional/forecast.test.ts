2cded67282856c9c3ecefb08682dfae06a4f351c
iamthepoe <luca1901p@gmail.com>
2023-05-25 20:16:38 -0300

    GET /forecast test

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,47 +1,57 @@
 const URL = 'http://localhost:3000';
 import assert from 'assert/strict';
-import { describe, it, before } from "node:test";
+import { describe, it, before, after } from "node:test";
+import { setupServer, dropServer } from '../setup/setup.ts';
 
-before(()=>{
-  
-});
+describe("tests", ()=>{
 
-describe("Beach forecast functional tests", ()=>{
-  it("should return status code 200", async ()=>{
-    const {body, status} = await fetch(`${URL}/forecast`);
-    assert.strictEqual(body, [{
-      "time": "2020-04-26T00:00:00+00:00",
-      "forecast": [{
-        "lat": -33.792726,
-        "lng": 151.289824,
-        "name": "Manly",
-        "position": "E",
-        "rating": 2,
-        "swellDirection": 64.26,
-        "swellHeight": 0.15,
-        "swellPeriod": 3.89,
+  before(()=>{
+    setupServer(3000);
+  });
+
+  after(()=>{
+    dropServer();
+  });
+
+  describe("Beach forecast functional tests", ()=>{
+    it("should return a forecast with just a few times", async ()=>{
+      const res = await fetch(`${URL}/forecast`);
+      const data = await res.json();
+
+      assert.strictEqual(res.status, 200);
+      assert.deepStrictEqual(data, [{
         "time": "2020-04-26T00:00:00+00:00",
-        "waveDirection": 231.38,
-        "waveHeight": 0.47,
-        "windDirection": 299.45
-      }]},
-      {
-      "time": "2020-04-26T01:00:00+00:00",
-      "forecast": [{
-        "lat": -33.792726,
-        "lng": 151.289824,
-        "name": "Manly",
-        "position": "E",
-        "rating": 2,
-        "swellDirection": 123.41,
-        "swellHeight": 0.21,
-        "swellPeriod": 3.67,
+        "forecast": [{
+          "lat": -33.792726,
+          "lng": 151.289824,
+          "name": "Manly",
+          "position": "E",
+          "rating": 2,
+          "swellDirection": 64.26,
+          "swellHeight": 0.15,
+          "swellPeriod": 3.89,
+          "time": "2020-04-26T00:00:00+00:00",
+          "waveDirection": 231.38,
+          "waveHeight": 0.47,
+          "windDirection": 299.45
+        }]},
+        {
         "time": "2020-04-26T01:00:00+00:00",
-        "waveDirection": 232.12,
-        "waveHeight": 0.46,
-        "windDirection": 310.48
-      }]}
-    ]);
-    assert.strictEqual(status, 200);
+        "forecast": [{
+          "lat": -33.792726,
+          "lng": 151.289824,
+          "name": "Manly",
+          "position": "E",
+          "rating": 2,
+          "swellDirection": 123.41,
+          "swellHeight": 0.21,
+          "swellPeriod": 3.67,
+          "time": "2020-04-26T01:00:00+00:00",
+          "waveDirection": 232.12,
+          "waveHeight": 0.46,
+          "windDirection": 310.48
+        }]}
+      ]);
+    });
   });
 });
