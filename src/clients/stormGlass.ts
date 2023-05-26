93696284ece3fba10789f90fd018e324c22f3f03
iamthepoe <luca1901p@gmail.com>
2023-05-25 21:07:11 -0300

    add api params to the client

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,11 @@
 export class StormGlass {
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+
+  constructor(protected request: typeof fetch){}
+
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
+    this.request("", {})
     return Promise.resolve({});
   }
 }
