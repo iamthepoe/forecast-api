429f697a79918dbd10a6ee0380ab8433fd3bc088
iamthepoe <luca1901p@gmail.com>
2023-05-25 21:12:30 -0300

    fix url

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -2,11 +2,12 @@ export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
   readonly stormGlassAPISource = 'noaa';
-
-  constructor(protected request: typeof fetch) {}
+  
+  constructor(protected request: typeof fetch){}
 
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    this.request('', {});
-    return Promise.resolve({});
+    return this.request(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`, {
+      method: 'GET'
+    });
   }
 }
