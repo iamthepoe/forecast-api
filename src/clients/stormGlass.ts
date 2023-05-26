434e520d7fdd71264602ac724096e00400d25c7e
iamthepoe <luca1901p@gmail.com>
2023-05-25 21:08:45 -0300

    add api source to the client

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,7 +1,8 @@
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
-
+  readonly stormGlassAPISource = 'noaa';
+  
   constructor(protected request: typeof fetch){}
 
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
