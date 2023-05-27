4456961d7913e54674b420a1bbe579a36dd07de8
iamthepoe <luca1901p@gmail.com>
2023-05-27 17:46:35 -0300

    create factory function

--- test/factories/fetch.mock.factory.ts
+++ test/factories/fetch.mock.factory.ts
@@ -1,4 +1,16 @@
+import { mock } from "node:test";
+
 type FetchMockConfig = {
   responseData: any;
 };
 
+const createFetchMock = ({ responseData }: FetchMockConfig) => {
+    return mock.fn(fetch, (input: RequestInfo | URL, init?: RequestInit | undefined )=>{
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(responseData),
+      });
+    }) as typeof fetch;
+};
+
+export {createFetchMock};
