e9c672d8fd310d50e5ccfcb8e63027922e4749ab
iamthepoe <luca1901p@gmail.com>
2023-05-27 18:47:02 -0300

    add StormGlassPoint interface

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -2,6 +2,17 @@ export interface StormGlassPointSource{
   [key: string]: number;
 }
 
+export interface StormGlassPoint{
+  readonly time: string;
+  readonly waveHeight: StormGlassPointSource;
+  readonly waveDirection: StormGlassPointSource;
+  readonly swellDirection: StormGlassPointSource;
+  readonly swellHeight: StormGlassPointSource;
+  readonly swellPeriod: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
+}
+
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
