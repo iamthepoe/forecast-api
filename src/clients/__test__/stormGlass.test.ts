58b1bf3e64c3d7406214d305ea309dcb469f4542
iamthepoe <luca1901p@gmail.com>
2023-05-27 18:34:04 -0300

    add normalized fixture to assert comparation

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,5 +1,5 @@
 import assert from 'assert/strict';
-import { describe, it} from 'node:test';
+import { describe, it } from 'node:test';
 import { StormGlass } from '../stormGlass.ts';
 import { createFetchMock } from '../../../test/factories/fetch.mock.factory.ts';
 import stormglassNormalizedResponseFixture from '../../../test/fixtures/stormglass_normalized_response_3_hours.json';
@@ -7,12 +7,14 @@ import * as stormglassWeatherPointFixture from '../../../test/fixtures/stormglas
 
 describe('StormGlass Client', () => {
   it('should return normalized forecast from the StormGlass service', async () => {
-    const fetchMock = createFetchMock({responseData: {data: stormglassWeatherPointFixture}});
+    const fetchMock = createFetchMock({
+      responseData: { data: stormglassWeatherPointFixture },
+    });
     const lat = -33.792726;
     const lng = 151.721726;
 
     const stormGlass = new StormGlass(fetchMock);
     const response = await stormGlass.fetchPoints(lat, lng);
-    assert.deepStrictEqual(response, stormglassWeatherPointFixture);
+    assert.deepStrictEqual(response, stormglassNormalizedResponseFixture);
   });
 });
