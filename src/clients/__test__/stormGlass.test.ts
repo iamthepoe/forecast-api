453eba1183ed187fa9f761f6a91b6341260fae5b
iamthepoe <luca1901p@gmail.com>
2023-05-27 17:52:53 -0300

    add factory to test

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,21 +1,13 @@
 import assert from 'assert/strict';
-import { describe, it, mock } from 'node:test';
+import { describe, it} from 'node:test';
 import { StormGlass } from '../stormGlass.ts';
+import { createFetchMock } from '../../../test/factories/fetch.mock.factory.ts';
 import stormglassNormalizedResponseFixture from '../../../test/fixtures/stormglass_normalized_response_3_hours.json';
 import * as stormglassWeatherPointFixture from '../../../test/fixtures/stormglass_weather_3_hours.json';
 
 describe('StormGlass Client', () => {
   it('should return normalized forecast from the StormGlass service', async () => {
-    const fetchMock = mock.fn(
-      fetch,
-      (input: RequestInfo | URL, init?: RequestInit | undefined) => {
-        return Promise.resolve({
-          ok: true,
-          json: () => Promise.resolve({ data: stormglassWeatherPointFixture }),
-        });
-      }
-    ) as typeof fetch;
-
+    const fetchMock = createFetchMock({responseData: {data: stormglassWeatherPointFixture}});
     const lat = -33.792726;
     const lng = 151.721726;
 
